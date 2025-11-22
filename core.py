c4b59ab9cb79854ec3572742907a802899f72a1f
iB <iig388pw@gmail.com>
2025-11-22 13:59:49 +0100

    Changed core.py for Render.com, now DataBase will working on Render's PostgreSQL

--- core.py
+++ core.py
@@ -1,10 +1,14 @@
 from aiogram import Router, F
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from config.config import DATABASE_URL
-from models import Base
 
 router = Router()
+
+LOCAL_DB = 'sqlite+aiosqlite:///./database/workhelper.db'
+
+# База для деплоя
+DATABASE_URL = os.getenv('DATABASE_URL', LOCAL_DB)
 
 # Асинхронный движок SQLAlchemy
 engine = create_async_engine(DATABASE_URL, echo=True)
