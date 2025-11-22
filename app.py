d34281abed564504cebdb2262e546c18cb623283
iB <iig388pw@gmail.com>
2025-11-22 13:53:21 +0100

    Full delete 'Config', edited app.py for deploy on Render.com, added PostgreSQL on Render and created database.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.storage.memory import MemoryStorage
-from config.config import TOKEN, DATABASE_URL
 from models import Base
 from core import router, async_session, engine
 import handlers.handlers
+
+TOKEN = os.getenv("BOT_TOKEN")
+LOCAL_DB = 'sqlite+aiosqlite:///./database/workhelper.db'
+DATABASE_URL = os.getenv("DATABASE_URL", LOCAL_DB)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
